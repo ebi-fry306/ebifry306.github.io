6e0f45015bb205df523762894c8fbd16355c45f0
えびふらい <ebi_fry306@icloud.com>
2015-01-21 23:00:46 +0900

    整理整頓

--- css.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var UA = navigator.userAgent;
-    if (UA.indexOf('iPhone') > 0 || UA.indexOf('iPod') > 0 || UA.indexOf('Android') > 0){
-        document.write('<link href ="style-sp.css" type="text/css" rel="stylesheet"/>');
-    }
-    else{
-        document.write('<link href ="style-sheet.css" type="text/css" rel="stylesheet"/>');
-    }
-})();
