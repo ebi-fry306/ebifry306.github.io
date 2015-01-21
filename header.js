d783f1f061992233a2c32a52a6b43da39a574956
えびふらい <ebi_fry306@icloud.com>
2015-01-21 22:25:12 +0900

    フォント周りの調整

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
   (function () {
     var UA = navigator.userAgent;
     if (UA.indexOf('iPhone') > 0 || UA.indexOf('iPod') > 0 || UA.indexOf('Android') > 0){
-        document.write('<header><h1><div class="string">ラー油</div></h1></header>');
+        document.write('<header><h1>ラー油</h1></header>');
     }
     else{
-        document.write('<header><h1><div class="string">オリーブオイル</div></h1></header>');
+        document.write('<header><h1>オリーブオイル</h1></header>');
     }
 })();
