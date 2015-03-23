1d8b9558c42aa3d0d1561984f6648062f197ce8b
えびふらい <ebi_fry306@icloud.com>
2015-03-23 22:50:39 +0900

    update

--- js/header.js
+++
@@ -1,9 +0,0 @@
-  (function () {
-    var UA = navigator.userAgent;
-    if (UA.indexOf('iPhone') > 0 || UA.indexOf('iPod') > 0 || UA.indexOf('Android') > 0){
-        document.write('<header><h1>ラー油</h1></header>');
-    }
-    else{
-        document.write('<header><h1>オリーブオイル</h1></header>');
-    }
-})();
